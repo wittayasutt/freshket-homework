f8009eb0ea59fa151dc49f24ea428925ae524454
wittayasutt <wittayasutt@gmail.com>
2024-02-05 19:44:32 +0700

    feat/add container

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -5,7 +5,14 @@ type Props = {
 	children: ReactNode;
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+	min-height: 100vh;
+
+	@media (min-width: ${(props) => props.theme.breakpoints.desktop}) {
+		max-width: ${(props) => props.theme.breakpoints.desktop};
+		margin: auto;
+	}
+`;
 
 const Container = ({ children }: Props) => {
 	return <Wrapper>{children}</Wrapper>;
